82d9c3dc3dc946aea5fd6a4c1487bdc0ea673f93
“AlbertoAlpe” <alberto.alpe@edu.unito.it>
2024-05-19 00:41:54 +0200

    quasi terminato PoS probab

--- prova.py
+++ prova.py
@@ -1,4 +1,3 @@
-import numpy as np
 import csv
 import os
 file_name = 'train.conllu'
@@ -6,18 +5,18 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, 'wikineural_en', file_name)
 
 train = open(file_path, 'r', encoding='utf-8')
-prime_100_righe = train.readlines()[:1000]
+prime_100_righe = train.readlines()[:100]
 
 
-emission_P = [[0], [0]] 
-transition_P = [[0], [0]]
-#emission_P = [[], []] #la matrice iniziale è di dimensione 2x0, cioè ha due righe(i tag BEGIN e END) vuote; 
+emission_P = [[], []]           #matrice iniziale composta da due righe vuote
+transition_P = [[0, 0], [0, 0]]
+#emission_P = [[], []]
 #transition_P = [[], []]  
 tags = ['BEGIN', 'END']
 words = []
 word = 0
 tag = 0
-tag_prec = None
+tag_prec = 'BEGIN'
 tpi = 0
 
 #due funzioni per aggiungere dinamicamente una riga o una colonna alle matrici
@@ -39,20 +38,21 @@ for riga in prime_100_righe:
     riga = riga.strip()
     if riga:
         riga = riga.split()
-        if riga[2] not in tags:  #se il tag è nuovo, aggiorna le dimensioni di tags e delle due matrici 
-           tags.append(riga[2])
-           tag = len(tags) - 1
-           aggiungi_riga(transition_P)
-           aggiungi_riga(emission_P)
-        else:
-           tag = tags.index(riga[2]) 
-
         if riga[1] not in words:    #se la parola è nuova, aggiorna le dimensioni di words e di emission_P
            words.append(riga[1])
            word = len(words) - 1
            aggiungi_colonna(emission_P)
         else:   
            word = words.index(riga[1])
+
+        if riga[2] not in tags:  #se il tag è nuovo, aggiorna le dimensioni di tags e delle due matrici 
+           tags.append(riga[2])
+           tag = len(tags) - 1
+           aggiungi_riga(transition_P)
+           aggiungi_colonna(transition_P)
+           aggiungi_riga(emission_P)
+        else:
+           tag = tags.index(riga[2]) 
         
         #in tag abbiamo l'indice della riga in cui operare
         #in word abbiamo l'indice della parola in cui operare
@@ -60,9 +60,9 @@ for riga in prime_100_righe:
         #emission_P[riga[2]][riga[1]] = emission_P[riga[2]].get(riga[1], 0) + 1
         tpi = tags.index(tag_prec)
         transition_P[tpi][tag] += 1
-        tag_prec = riga[2]    
+        tag_prec = riga[2]
     else: 
-        #in questo caso il tag è END, si fanno i calcoli necessari e poi il tag_prec diventa BEGIN
+        #in questo caso la riga è vuota => il tag è END, si fanno i calcoli necessari e poi il tag_prec diventa BEGIN
         tpi = tags.index(tag_prec)
         transition_P[tpi][1] += 1  #indice 1 perchè END è il secondo elemento dell'array tags
         tag_prec = "BEGIN"         
@@ -76,16 +76,20 @@ print(transition_P)
 #in ogni riga avremo tutte le occorrenze di un tag divise per ogni parola, quindi le sommiamo in tagTot
 #la variabile tagTot verrà usata nei due cicli for incapsulati nel primo, 
 #per calcolare le probabilità di emissione e tansizione
-for riga in emission_P:
+for r, row in enumerate(emission_P):
    tagTot = 0
-   for i in len(riga)-1:
-      tagTot += emission_P[riga][i]
-   for j in len(riga)-1:   #CALCOLO PROB. EMISSIONE
-      emission_P[riga][j] /= tagTot
-   for t in range(11):     #CALCOLO PROB. TRANSIZIONE    
-      transition_P[riga][t] /= tagTot
+   for i in row:
+      tagTot += i
+   if (tagTot>0): 
+      for idx, value in enumerate(row):     #CALCOLO PROB. EMISSIONE
+            emission_P[r][idx] = value / tagTot
 
+      for t in transition_P[r]:      #CALCOLO PROB. TRANSIZIONE    
+         t = t/tagTot
 
+print(transition_P)
+
+'''
 #salva le matrici risultanti in due files .csv
 emiss_csv = "emissione_en.csv"
 transiz_csv = "transizione_en.csv"
@@ -97,3 +101,5 @@ with open(emiss_csv, mode='w', newline='') as emiss:
 with open(transiz_csv, mode='w', newline='') as transiz:
     writer = csv.writer(transiz)
     writer.writerows(transition_P)
+
+'''
